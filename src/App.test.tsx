00436c6312e52296453b1b7ad9cd466bd2fd235c
chordzz <olaifeolawore@gmail.com>
2022-10-25 22:05:15 +0100

    removedd winston and logger for now, to enable initial push

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders homepage", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const homepage = screen.getByTestId("homepage");
+  expect(homepage).toBeInTheDocument();
+});
