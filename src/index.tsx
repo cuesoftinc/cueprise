8a4204c4c067b069e52d635600d717afb839a2f3
Ibukun Dairo <ibukun.dairo@cuesoft.io>
2022-10-26 11:59:06 +0100

    update packages (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { WinstonProvider } from "winston-react";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import logger from "./libs/logger";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <WinstonProvider logger={logger}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </WinstonProvider>
   </React.StrictMode>
 );
 
