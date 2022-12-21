0b252361cfeb7b7ebce61b0e01106004fea8eff9
Olaife Olawore <52765542+chordzz@users.noreply.github.com>
2022-12-21 15:18:58 +0100

    Feature/cp 441 (#17)
    * added demo video popup to site
    
    * fixed modal z-index issue

--- src/components/custom-modal/CustomModal.tsx
+++ src/components/custom-modal/CustomModal.tsx
@@ -18,7 +18,7 @@ export const CustomModal = ({ openModal, closeModal }: myProps) => {
   return (
     <div
       id="modal-backdrop"
-      className="bg-black fixed inset-0 bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
+      className="bg-black fixed inset-0 bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-[99]"
       onClick={handleCloseModal}
     >
       <div className="w-[85%] h-[50%] md:w-1/2 md:h-1/2">
@@ -26,7 +26,7 @@ export const CustomModal = ({ openModal, closeModal }: myProps) => {
           className="w-full h-full"
           title="cueprise-product-demo-video"
           name="demo-iframe"
-          src="https://www.youtube.com/embed/WiaBlUBXvY8"
+          src="https://www.youtube.com/embed/WiaBlUBXvY8?&autoplay=1"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture full"
           allowFullScreen
         ></iframe>
