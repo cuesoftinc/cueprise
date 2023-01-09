e71f72c118ffaee57395d2335b8ebe279332ec0b
Olaife Olawore <52765542+chordzz@users.noreply.github.com>
2023-01-09 22:36:38 +0100

    added 404 page to cueprise website (#25)

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/navigation-bar/Navbar";
 
 import { Homepage } from "./pages/Homepage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <>
           <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </>
       </Routes>
       <Footer />
