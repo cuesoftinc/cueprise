88d280890982f1cf4d30fc85e4bc3d84636dd59a
Olaife Olawore <olaifeolawore@gmail.com>
2023-02-24 11:52:17 +0100

    [CP-442] removed all dummy text from website

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,16 +14,19 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        heroEllipseLeft: "url('./assets/others/hero-ellipse-background-left.svg')",
-        heroEllipseRight: "url('./assets/others/hero-ellipse-background-right.svg')",
-        screenSectionBg: "url('./assets/others/screens-section-background.svg')",
+        heroEllipseLeft:
+          "url('./assets/others/hero-ellipse-background-left.svg')",
+        heroEllipseRight:
+          "url('./assets/others/hero-ellipse-background-right.svg')",
+        screenSectionBg:
+          "url('./assets/others/screens-section-background.svg')",
         footerBgLine: "url('./assets/vectors/footer-line-vector.svg')",
         pricingWavyBg: "url('./assets/others/wavy-bg.svg')"
       },
       colors: {
-        bgPrimary: '#B888F1',
-        textPrimary: '#475569',
-        textHeader: '#64748B'
+        bgPrimary: "#B888F1",
+        textPrimary: "#475569",
+        textHeader: "#64748B"
       }
     },
     fontFamily: {
